corneltek/MacPorts

commit 628c20968726f62bc77b41fba98de1fe30618574

darwintrace: comment unused code
git-svn-id: http://svn.macports.org/repository/macports/trunk@112610 d073be05-634f-4543-b044-5fe20cf6d1d6

base/src/darwintracelib1.0/darwintrace.c
@@ -622,14 +622,16 @@ static inline void __darwintrace_setup() {
  * \param[in] path the (not necessarily absolute) path to send to tracelib
  * \param[in] fd a FD to the file, or 0, if none available
  */
-static inline void __darwintrace_log_op(const char *op, const char *path, int fd) {
+static inline void __darwintrace_log_op(const char *op, const char *path) {
 	uint32_t size;
 	char pathbuf[MAXPATHLEN];
 	char logbuffer[BUFFER_SIZE];
 	const char *realpath;
 
 	do {
+#		if 0
 #       ifdef __APPLE__ /* Only Darwin has volfs and F_GETPATH */
+		/* TODO Use getattrlist(2) ATTR_CMN_NAME to get this */
 		if ((fd > 0) && (strncmp(path, "/.vol/", 6) == 0)) {
 			if (fcntl(fd, F_GETPATH, pathbuf) != -1) {
 				realpath = pathbuf;
@@ -637,6 +639,7 @@ static inline void __darwintrace_log_op(const char *op, const char *path, int fd
 			}
 		}
 #       endif
+#		endif
 
 		if (*path != '/') {
 			if (!getcwd(pathbuf, sizeof(pathbuf))) {

commit 1f9ef8092feebc0ed99a7b69886909c95f198348

darwintrace: fix build errors introduced by r112610
git-svn-id: http://svn.macports.org/repository/macports/trunk@112611 d073be05-634f-4543-b044-5fe20cf6d1d6

base/src/darwintracelib1.0/darwintrace.c
@@ -111,7 +111,7 @@ size_t strlcpy(char *dst, const char *src, size_t size) {
 #define DARWINTRACE_DEBUG (0)
 #endif
 
-static inline void __darwintrace_log_op(const char *op, const char *path, int fd);
+static inline void __darwintrace_log_op(const char *op, const char *path);
 static void __darwintrace_copy_env() __attribute__((constructor));
 static void __darwintrace_setup_tls() __attribute__((constructor));
 static inline char *__darwintrace_alloc_env(const char *varName, const char *varValue);
@@ -910,12 +910,12 @@ static inline int __darwintrace_is_in_sandbox(const char *path, char *newpath, b
 							 * access anyway, but report a sandbox violation.
 							 * TODO find a better solution */
 							if (report)
-								__darwintrace_log_op("sandbox_violation", normalizedpath, 0);
+								__darwintrace_log_op("sandbox_violation", normalizedpath);
 							return 1;
 						case 0:
 							/* file belongs to a foreign port, deny access */
 							if (report)
-								__darwintrace_log_op("sandbox_violation", normalizedpath, 0);
+								__darwintrace_log_op("sandbox_violation", normalizedpath);
 							return 0;
 					}
 				default:
@@ -926,7 +926,7 @@ static inline int __darwintrace_is_in_sandbox(const char *path, char *newpath, b
 	}
 
 	if (report)
-		__darwintrace_log_op("sandbox_violation", normalizedpath, 0);
+		__darwintrace_log_op("sandbox_violation", normalizedpath);
 	return 0;
 }
 